493334a2669765b4262f81361e9b0bdd28d78dc2
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-10 08:35:13 +0200

    add new item logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import './styles/App.css';
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-	const [posts, setProps] = useState([
+	const [posts, setPosts] = useState([
 		{id: 1, title: 'JavaScript 1', body: 'Description'},
 		{id: 2, title: 'JavaScript 2', body: 'Description'},
 		{id: 3, title: 'JavaScript 3', body: 'Description'},
 	]);
+	const [post, setPost] = useState({title: '', body: ''});
+
+	const addNewPost = (e) => {
+		e.preventDefault();
+
+		setPosts([...posts, {...post, id: Date.now()}]);
+		setPost({title: '', body: ''});
+	}
 
 	return (
 		<div className="App">
+			<form>
+				<MyInput
+					type='text'
+					placeholder='Название поста'
+					value={post.title}
+					onChange={(e) => setPost({...post, title: e.target.value})}
+				/>
+				<MyInput
+					type='text'
+					placeholder='Описание поста'
+					value={post.body}
+					onChange={(e) => setPost({...post, body: e.target.value})}
+				/>
+				<MyButton onClick={addNewPost}>Создать пост</MyButton>
+			</form>
 			<PostList posts={posts} title='Список постов 1'/>
 		</div>
 	);
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,8 +7,8 @@ const PostList = ({posts, title}) => {
 			<h1 style={{textAlign: 'center'}}>
 				{title}
 			</h1>
-			{posts.map(post => {
-				return <PostItem post={post} key={post.id}/>
+			{posts.map((post, index) => {
+				return <PostItem number={index + 1} post={post} key={post.id}/>
 			})}
 		</div>
 	);
