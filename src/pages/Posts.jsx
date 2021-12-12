a990ad45d9de3bcb5cb147c8cb5d81de3861722d
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-12 23:50:55 +0200

    add infinite scroll

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import PostList from "../components/PostList";
 import PostForm from "../components/PostForm";
 import PostFilter from "../components/PostFilter";
@@ -10,30 +10,33 @@ import Loader from "../components/UI/Loader/Loader";
 import {useFetching} from "../hooks/useFetching";
 import {getPageCount} from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
+import {useObserver} from "../hooks/useObserver";
+import MySelect from "../components/UI/Select/MySelect";
 
 function Posts() {
-	const [posts, setPosts] = useState([
-		// {id: 1, title: 'b', body: 'c'},
-		// {id: 2, title: 'a', body: 'b'},
-		// {id: 3, title: 'c', body: 'a'},
-	]);
+	const [posts, setPosts] = useState([]);
 	const [filter, setFilter] = useState({sort: '', query: ''});
 	const [modal, setModal] = useState(false);
 	const [totalPages, setTotalPages] = useState(0);
 	const [limit, setLimit] = useState(10);
 	const [page, setPage] = useState(1);
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+	const lastElement = useRef();
 
 	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
 		const response = await PostService.getAll(limit, page);
-		setPosts(response.data);
+		setPosts([...posts, ...response.data]);
 		const totalCount = response.headers['x-total-count'];
 		setTotalPages(getPageCount(totalCount, limit));
 	});
 
+	useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+		setPage(page + 1);
+	})
+
 	useEffect(() => {
 		fetchPosts();
-	}, [page]);
+	}, [page, limit]);
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
@@ -60,12 +63,20 @@ function Posts() {
 			</MyModal>
 			<hr style={{margin: '15px 0'}}/>
 			<PostFilter filter={filter} setFilter={setFilter}/>
+			<MySelect value={limit} onChange={value => setLimit(value)}
+								defaultValue='Кол-во элементов на странице' options={[
+				{value: 5, name: '5'},
+				{value: 10, name: '10'},
+				{value: 25, name: '25'},
+				{value: -1, name: 'Показать все'},
+			]}/>
 			{postError && <h1>Произошла ошибка!</h1>}
-			{isPostsLoading
-				? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
-					<Loader/></div>
-				: <PostList remove={removePost} posts={sortedAndSearchedPosts}
-										title='Список постов 1'/>}
+			<PostList remove={removePost} posts={sortedAndSearchedPosts}
+								title='Список постов'/>
+			<div ref={lastElement}/>
+			{isPostsLoading &&
+				<div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
+					<Loader/></div>}
 			<Pagination page={page} changePage={changePage} totalPages={totalPages}/>
 		</div>
 	);
